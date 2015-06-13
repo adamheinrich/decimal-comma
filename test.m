af5a8fe015c31a9be7ffe4dd00b7006a93441aeb
Adam Heinrich <adam@adamh.cz>
2015-06-13 17:40:47 +0200

    Extend test script to try multiple options of decimal_comma.m.

--- test.m
+++ test.m
@@ -6,11 +6,31 @@ x = -1:0.1:1;
 y = x.^3;
 
 figure;
-plot(x, y);
 
-grid on;
-title('Simple plot');
-ylabel('y=x^3');
-xlabel('x');
+for i=1:4
+    subplot(2, 2, i);
+    plot(x, y);
 
-decimal_comma(gca, 'XY');
+    grid on;
+    title([ 'Simple plot #', num2str(i) ]);
+    ylabel('y=x^3');
+    xlabel('x');
+
+    switch i
+        case 1
+            decimal_comma(gca, 'XY');
+            
+       case 2
+            decimal_comma(gca, 'Y');
+            
+       case 3
+            decimal_comma(gca, 'XY', '%1.2f');
+            
+       case 4
+            decimal_comma(gca, 'X', '%1.2f');
+            decimal_comma(gca, 'Y');
+            
+        otherwise
+            
+    end
+end
